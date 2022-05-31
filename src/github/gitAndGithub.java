7d9b4e7e61f698dd9b6c57715c933383914f6b83
munevver44 <munevver1421@gmail.com>
2022-05-31 19:58:44 -0400

    first commit

---
+++ src/github/gitAndGithub.java
@@ -0,0 +1,11 @@
+package github;
+
+public class gitAndGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("today we learn github");
+System.out.println("taday is a very good day");
+	}
+
+}
